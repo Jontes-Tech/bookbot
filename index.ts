9ed5a86a26e56099afa57dc98db9f489f0f9503a
Jonatan Holmgren <jonatan@jontes.page>
2022-09-19 18:52:45 +0200

    redo stuff

--- index.ts
+++ index.ts
@@ -1,28 +1,18 @@
-// Read a file and print its contents.
-
 import fs from "fs";
 import axios from "axios"
 const config = require("./config.json")
-const book = fs.readFileSync(config.book, "utf8").split(/[!?.]/gm);
+const book = fs.readFileSync(`./books/${config.book}`, "utf8").split(/[!?.]/gm);
 const sentence = Number(fs.readFileSync("sentence.txt", "utf8"));
 
 console.log("BookBot By Jonte")
 
-const options = {
-  method: 'POST',
-  url: `${config.webhook}`,
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  data: {username: 'BookBot', content: `"${book[sentence].trim()}"`}
-};
 const refillOptions = {
   method: 'POST',
   url: `${config.webhook}`,
   headers: {
     'Content-Type': 'application/json'
   },
-  data: {username: 'BookBot', content: `📖 <@${config.owner}> Book is over, please refill the program with more text. Contact Jonte for details.`}
+  data: { username: 'BookBot', content: `📖 <@${config.owner}> Book is over, please refill the program with more text. Contact Jonte for details.` }
 };
 if (book[sentence] === undefined || book[sentence] === "") {
   axios.request(refillOptions).then(function (response) {
@@ -32,12 +22,21 @@ if (book[sentence] === undefined || book[sentence] === "") {
     console.error(error);
   });
 }
+else {
 
-
-axios.request(options).then(function (response) {
-  console.log("Successfully sent with code: "+response.status);
-}).catch(function (error) {
-  console.error(error);
-});
+  const options = {
+    method: 'POST',
+    url: `${config.webhook}`,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    data: { username: 'BookBot', content: `"${book[sentence].trim()}"` }
+  };
+  axios.request(options).then(function (response) {
+    console.log("Successfully sent with code: " + response.status);
+  }).catch(function (error) {
+    console.error(error);
+  });
+}
 
 fs.writeFileSync("sentence.txt", String(sentence + 1));
